6b29ed473c18c3ed5e47deefb7cd029dba12c451
xie_s <xie_s@worksap.co.jp>
2013-08-15 16:19:23 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ICMS::Application.config.secret_token = 'ca1c1f15f8b0990778ddd74e2d59b65f3e7e0a3f204afa33bd377b3efdff093f560ef9f9b57756cf77ae25d512becd47ec97fe0d6dc751274047db370b63bb3c'
