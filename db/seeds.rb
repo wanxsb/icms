54427e1abbc8694dd4352efe1f96c53aa812e46d
xie_s <xie_s@isumeng.com>
2013-08-21 00:35:11 +0800

    初步实现首页

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -17,3 +18,12 @@ Refinery::Inquiries::Engine.load_seed
 
 # Added by Refinery CMS Products extension
 Refinery::Products::Engine.load_seed
+
+homePage = Refinery::Page.where(:slug == 'home')
+if !homePage.blank?
+	photo = Refinery::Image.new
+	photo.image = File.open(File.join(Rails.root, "/app/assets/images/down.png"))
+	photo.save!
+	pagePart = Refinery::PagePart.find_by_id(1)
+	pagePart.update_attribute(:body, "<div id=\"introduction1\"><span class=\"white\">企业应用软件</span><strong class=\"light-blue\">专家</strong></div><div id=\"introduction2\"><span class=\"sky-blue\">企业网站/购物商城/客户关系管理/GPS定位预警</span></div><hr /><div id=\"introduction3\"><span class=\"white-blue\">“塑梦科技”帮助中小型企业打造互联网企业商务平台，推进企业智能化、信息化建设，助力有梦想的合作伙伴挺进新一轮信息革命的</span><span class=\"white\">浪潮之巅</span></div><div id=\"introduction4\"><img src=\"#{photo.url}\" rel=\"225x255\" width=\"45\" height=\"44\" /></div>" )
+end
--- vendor/extensions/products/db/seeds.rb
+++ vendor/extensions/products/db/seeds.rb
@@ -23,15 +23,57 @@
       page.parts.create(:title => default_page_part, :body => nil, :position => index)
     end
   end
-  
-  photo = Refinery::Image.new
-  photo.image = File.open("#{Rails.root}/app/assets/images/demo/demo-shop-small.png")
+  photos = []
+  photo	= Refinery::Image.new
+  photo.image = File.open(File.join(Rails.root, "/app/assets/images/demo/demo-jhmy.jpg"))
   photo.save!
-  
+  photos << photo
+
+  photo	= Refinery::Image.new
+  photo.image = File.open(File.join(Rails.root, "/app/assets/images/demo/demo-shida.jpg"))
+  photo.save!
+  photos << photo
+
+  photo	= Refinery::Image.new
+  photo.image = File.open(File.join(Rails.root, "/app/assets/images/demo/demo-bokche.jpg"))
+  photo.save!
+  photos << photo
+
+  photo	= Refinery::Image.new
+  photo.image = File.open(File.join(Rails.root, "/app/assets/images/demo/demo-es8bb-com.jpg"))
+  photo.save!
+  photos << photo
+
+  description = ""
+  photos.each do |singlePhoto|
+	   description += "<img src=#{singlePhoto.url}/>"
+  end
+
+
+  product= Refinery::Products::Product.create!( 
+    :name => "iCMS建站系统",
+    :price => 3600,
+    :abstract => "易于入手的后台管理，漂亮美观的前台界面，专为搜索引擎所做的URL链接优化，让客户能够迅速的锁定您的企业作为其合作伙伴",
+    :description  => description,
+    :detail => "是一套面向中小传统企业推出的零售直销型全网营销快速解决方案，帮企业花少量投资，快速拥有属于自己的全网营销型官网商城，助力企业实现多渠道同步营销，跨平台同步管理，让企业快速、高效开启电子商务之路。电商云 <ul> <li>企业级| 稳定卓越官网</li><li>商城+淘宝+移动互联</li><li>多元化营销 | 辐射全网</li><li>多平台统一管理 | 高效运营</li></ul>",
+    :showOnIndex => true,
+    :position => 2
+    )
+
+  photos.each do |singlePhoto|
+	  imagePage= Refinery::ImagePage.new
+	  imagePage.image = singlePhoto
+	  imagePage.page = product
+	  imagePage.save!
+  end
+
+  photo = Refinery::Image.new
+  photo.image = File.open(File.join(Rails.root, "/app/assets/images/demo/demo-shop-small.png"))
+  photo.save!
   
   product= Refinery::Products::Product.create!( 
     :name => "iStore网上商城",
-    :price => 3600,
+    :price => 6800,
     :abstract => "灵活的促销方式，让您随意的改变促销规则；完善的购物车,强大的会员中心系统；支持支付宝,财付通等网上支付通道；",
     :description  => "<img src=#{photo.url}/>",
     :detail => "是一套面向中小传统企业推出的零售直销型全网营销快速解决方案，帮企业花少量投资，快速拥有属于自己的全网营销型官网商城，助力企业实现多渠道同步营销，跨平台同步管理，让企业快速、高效开启电子商务之路。电商云 <ul> <li>企业级| 稳定卓越官网</li><li>商城+淘宝+移动互联</li><li>多元化营销 | 辐射全网</li><li>多平台统一管理 | 高效运营</li></ul>",
